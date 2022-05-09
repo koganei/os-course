54719cbd8d6eb2e70bbbebcf851df57a6b7f93ee
Marc Khoury <khoury.marc@gmail.com>
2022-05-09 08:37:05 -0400

    first commit

---
+++ Chapter 2/copy_file.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <fstream>
+#include <errno.h>
+// extern int errno;
+
+using namespace std;
+
+void getSource(char* source) {
+    printf("Path of source: ");
+    scanf("%119s", source);
+}
+
+void getDestination(char* dest) {
+    printf("Path of destination: ");
+    scanf("%119s", dest);
+}
+
+int main() {
+    char data;
+    char source[120], dest[120];
+    ifstream sourceStream;
+    ofstream destinationStream;
+
+    getSource(source);
+    sourceStream.open(source, ios::in);
+
+    if(!sourceStream) {
+        perror("Couldn't open file for reading");
+        return 1;
+    }
+
+    getDestination(dest);
+    destinationStream.open(dest, ios::out);
+
+    while(!sourceStream.eof()) {
+        sourceStream >> noskipws >> data;
+        destinationStream << data;
+    }
+
+    sourceStream.close();
+    destinationStream.close();
+    
+ 
+
+    printf("Program Completed.\n");
+    return 0;
+}
